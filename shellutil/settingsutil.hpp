ba10142a98815120e657274293c059786ebb14cb
sadanslargehole <sadan@sadanslargehole.com>
2024-04-12 22:04:08 -0400

    added unicode escaping

--- shellutil/settingsutil.hpp
+++ shellutil/settingsutil.hpp
@@ -44,7 +44,44 @@ inline std::string escapeToReadable(const std::string &input) {
     }
     return result.str();
 }
-
+inline char *getUnicodeChar(unsigned int code) {
+    char *chars = new char[5]();
+    if (code <= 0x7F) {
+        chars[0] = (code & 0x7F);
+        chars[1] = '\0';
+    } else if (code <= 0x7FF) {
+        // one continuation byte
+        chars[1] = 0x80 | (code & 0x3F);
+        code = (code >> 6);
+        chars[0] = 0xC0 | (code & 0x1F);
+        chars[2] = '\0';
+    } else if (code <= 0xFFFF) {
+        // two continuation bytes
+        chars[2] = 0x80 | (code & 0x3F);
+        code = (code >> 6);
+        chars[1] = 0x80 | (code & 0x3F);
+        code = (code >> 6);
+        chars[0] = 0xE0 | (code & 0xF);
+        chars[3] = '\0';
+    } else if (code <= 0x10FFFF) {
+        // three continuation bytes
+        chars[3] = 0x80 | (code & 0x3F);
+        code = (code >> 6);
+        chars[2] = 0x80 | (code & 0x3F);
+        code = (code >> 6);
+        chars[1] = 0x80 | (code & 0x3F);
+        code = (code >> 6);
+        chars[0] = 0xF0 | (code & 0x7);
+        chars[4] = '\0';
+    } else {
+        // unicode replacement character
+        chars[2] = 0xEF;
+        chars[1] = 0xBF;
+        chars[0] = 0xBD;
+        chars[3] = '\0';
+    }
+    return chars;
+}
 inline std::string readableToEscape(const std::string &input) {
     std::ostringstream result;
     size_t i = 0;
@@ -52,6 +89,15 @@ inline std::string readableToEscape(const std::string &input) {
         if (input[i] == '\\' && input[i + 1] == 'e') {
             result << '\033';
             i += 2;
+        } else if (input[i] == '\\' && input[i + 1] == 'u') {
+            std::string chars = "";
+            chars += input[i + 2];
+            chars += input[i + 3];
+            chars += input[i + 4];
+            chars += input[i + 5];
+            i += 6;
+            int codepoint = std::stoi(chars, nullptr, 16);
+            result << getUnicodeChar(codepoint);
         } else if (input[i] == '\\' && input[i + 1] == ';') {
             result << ';';
             i += 2;
